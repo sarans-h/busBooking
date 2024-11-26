4401b263fd38c34f6488f7d79bbacbd2d8ddc179
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2024-11-26 22:51:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-qr-reader': 'react-qr-reader'
-    }
-  },
   server: {
     proxy: {
       '/api': {
