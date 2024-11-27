4249eb8eecc714834614c90a5f79804cc3ef484b
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2024-11-27 11:06:16 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,6 +5,7 @@ const sendToken=(user,statusCode,res)=>{
             Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000
         ),
         httpOnly:true,
+        secure: true,
         sameSite: "none",
     }
     res.status(statusCode).cookie('token',token,options).json({
