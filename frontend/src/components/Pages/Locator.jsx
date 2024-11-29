2a004afa5426037cc3b97abfd2266fbf9d027300
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2024-11-29 17:56:30 +0530

    Update Locator.jsx

--- frontend/src/components/Pages/Locator.jsx
+++ frontend/src/components/Pages/Locator.jsx
@@ -21,7 +21,7 @@ const Locator = () => {
   useEffect(() => {
     const fetchLocationByIP = async () => {
       try {
-        const response = await fetch(`http://ip-api.com/json/`);
+        const response = await fetch(`https://ipapi.co/json/`);
         if (!response.ok) {
           throw new Error("Failed to fetch location data");
         }
